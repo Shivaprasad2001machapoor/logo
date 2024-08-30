ae07c65f53038b507681b99666dcc401d9971d16
Shivaprasad <shivaprasadnomula75@gmail.com>
2024-08-30 17:07:07 +0530

    Initial commit

---
+++ src/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Logo.css';
+
+function Logo() {
+    return (
+        <div className="rhombus">
+            <div className="text">
+                <p className="main-text">HTML <span className="and-symbol">&</span> CSS</p>
+                <p className="sub-text">Design and Build</p>
+            </div>
+        </div>
+    );
+}
+
+export default Logo;
